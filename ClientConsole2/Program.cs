9b6c12876741f5efbc0bd036e652f627f685ed25
Jan Daniel Andersen (JAXX) <jaxx@oticon.com>
2019-01-16 12:27:37 +0100

    Multi client. Connected. Disconnected. Message. Join Group. Tell Group.

---
+++ ClientConsole2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ClientConsole2
+{
+    internal class Program
+    {
+        const string appName = "Client console 2";
+
+        private static void Main(string[] args)
+        {
+            var connection = new HubConnectionBuilder().WithUrl("https://localhost:44318/msg").Build();
+
+            connection.On("ReceiveMessage", (string sender, string message) => Console.WriteLine($"{sender} says: {message}"));
+
+            connection.Closed += async error =>
+            {
+                Console.WriteLine(error);
+                await connection.StartAsync();
+            };
+
+            connection.StartAsync().Wait();
+
+            var exitKey = string.Empty;
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line == "/q")
+                {
+                    break;
+                }
+
+                var joinMatch = new Regex("/j (.+)");
+                if (joinMatch.IsMatch(line))
+                {
+                    var groupName = joinMatch.Match(line).Groups[1].ToString();
+                    // Console.WriteLine("join " + groupName);
+                    connection.SendCoreAsync("AddToGroup", new[] {appName, groupName});
+                    continue;
+                }
+                
+                var tellMatch = new Regex("/t ([^ ]+) (.+)");
+                if (tellMatch.IsMatch(line))
+                {
+                    var groupName = tellMatch.Match(line).Groups[1].ToString();
+                    var message = tellMatch.Match(line).Groups[2].ToString();
+                    connection.SendCoreAsync("SendGroup", new[] {appName, groupName, message});
+                    continue;
+                }
+
+
+
+                connection.SendCoreAsync("Broadcast", new[] {appName, line});
+            }
+        }
+    }
+}
---
+++ MessageHub/DefaultHub.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MessageHub
+{
+    public class DefaultHub : Hub
+    {
+        const string appName = "Server DefaultHub";
+
+
+        public async Task Broadcast(string sender, string message)
+        {
+            var senderDecorated = sender + " [" + Context.ConnectionId+ "]";
+            await Clients.All.SendAsync("ReceiveMessage", senderDecorated, message);
+        }
+
+        public async Task AddToGroup(string sender, string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            var senderDecorated = sender + " [" + Context.ConnectionId+ "] joined group: " + groupName;
+            
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", appName, senderDecorated);
+        }
+
+        public async Task SendGroup(string sender, string groupName, string message)
+        {
+            var senderDecorated = sender + " [" + Context.ConnectionId+ "]";
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", senderDecorated, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var connection = "[" + Context.ConnectionId+ "] Client connected";
+            await Clients.Caller.SendAsync("ReceiveMessage", appName, connection);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            var connection = "[" + Context.ConnectionId+ "] Client disconnected";
+            await Clients.Caller.SendAsync("ReceiveMessage", appName, connection);
+        }
+    }
+}
